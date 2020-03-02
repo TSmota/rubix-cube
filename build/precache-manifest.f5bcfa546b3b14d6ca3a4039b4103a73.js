578f37da93053aab2ef33ed7e7e52597d05a57c8
Thiago <thiagomotasilva2000@gmail.com>
2020-03-01 21:18:25 -0300

    Pasta build

---
+++ build/precache-manifest.f5bcfa546b3b14d6ca3a4039b4103a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d920ed90cab3eb70430984ac1e566831",
+    "url": "/index.html"
+  },
+  {
+    "revision": "454dac748bbc582025db",
+    "url": "/static/css/main.9567d550.chunk.css"
+  },
+  {
+    "revision": "ea56af51b3c072b7f032",
+    "url": "/static/js/2.6ee5087c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.6ee5087c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "454dac748bbc582025db",
+    "url": "/static/js/main.d87ce480.chunk.js"
+  },
+  {
+    "revision": "a5e17c2557306d0fd42c",
+    "url": "/static/js/runtime-main.3eee9c47.js"
+  }
+]);
